6f7a3c3fb9ef4e9e0b97b12820a7767c6add6b47
jrbenny35 <bennyjr169@gmail.com>
2020-09-22 16:54:16 -0500

    Fix imports.

--- tests/frontend/ui/pages/desktop/base.py
+++ tests/frontend/ui/pages/desktop/base.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 
 
 class Base(Page):
